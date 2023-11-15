ca6c019cfc0b8cc4b65c096920969345acc655b1
Muhammad Mubashar Nouman <mubashirrnouman@gmail.com>
2023-11-15 19:20:49 +0500

    Folder 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,86 @@
-import React, { useState } from 'react';
-import Person from './assets/components/Person';
-import './App.css';
+import React, { Component } from "react";
+import Person from "./assets/components/Person";
+import "./App.css";
 
-const App = () => {
-  const [personaState, setPersonaState] = useState({
+class App extends Component {
+  state = {
     persons: [
-      { name: 'Mubashar', age: 22 },
-      { name: 'Umar', age: 23 },
-      { name: 'Rehan', age: 21 },
+      {id: 'jhs', name: "Mubashar", age: 22 },
+      {id: 'js', name: "Umar", age: 23 },
+      {id: 'jh', name: "Rehan", age: 21 },
+      {id: 'hs', name: "Musham", age: 24 },
     ],
-  });
+    showPersons: false,
+  };
 
-  const switchNameHandler = () => {
-    setPersonaState({
-      persons: [
-        { name: 'Mubashar Nouman', age: 22 },
-        { name: 'Umar Tariq', age: 23 },
-        { name: 'Rehan Ashraf', age: 21 },
-      ],
+  namChangeHandler = (event) => {
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]}
+    person.name= event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons: persons
     });
   };
 
-  return (
-    <>
-      <h1>Hi, I am a React App</h1>
-      <p>App JS File</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personaState.persons[0].name} age={personaState.persons[0].age} />
-      <Person name={personaState.persons[1].name} age={personaState.persons[1].age}>
-        My Hobbies: Racing
-      </Person>
-      <Person name={personaState.persons[2].name} age={personaState.persons[2].age} />
-    </>
-  );
-};
+  deletePersonHandler = (personIndex) => {
+    // Create a copy of the original persons array
+    const persons = [...this.state.persons];
+
+    // Remove the person at the specified index
+    persons.splice(personIndex, 1);
+
+    // Update the state with the new persons array
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+    
+  };
+
+  render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={(event) => this.deletePersonHandler(index)}
+                key={person.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <>
+        <h1>Hi, I am a React App</h1>
+        <p>App JS File</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {persons}
+      </>
+    );
+  }
+}
 
 export default App;
--- src/assets/components/Person.jsx
+++ src/assets/components/Person.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Person.css'
 
 export default function Person(props) {
   return (
-    <div>
-      <p>I am {props.name} and I am {props.age} years old</p>
+    <div className='Person'>
+      {console.log('first')}
+      <Person name="zubair chuntyan"/>
+      <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
+      <button onClick={props.click}>Delete This Person's </button>
       <p>{props.children}</p>
+      <input type="text" onChange={props.change} value={props.name} />
     </div>
   )
 }
