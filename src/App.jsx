8faf36b0349b7d0e1a0a0b913dcbef7f87760614
Muhammad Mubashar Nouman <mubashirrnouman@gmail.com>
2023-11-16 16:56:13 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,25 @@ import "./App.css";
 class App extends Component {
   state = {
     persons: [
-      {id: 'jhs', name: "Mubashar", age: 22 },
-      {id: 'js', name: "Umar", age: 23 },
-      {id: 'jh', name: "Rehan", age: 21 },
-      {id: 'hs', name: "Musham", age: 24 },
+      { id: "jhs", name: "Mubashar", age: 22 },
+      { id: "js", name: "Umar", age: 23 },
+      { id: "jh", name: "Rehan", age: 21 },
+      { id: "hs", name: "Musham", age: 24 },
     ],
     showPersons: false,
   };
 
   namChangeHandler = (event) => {
-    const personIndex = this.state.persons.findIndex(p =>{
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
 
-    const person = {...this.state.persons[personIndex]}
-    person.name= event.target.value;
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
     this.setState({
-      persons: persons
+      persons: persons,
     });
   };
 
@@ -41,7 +41,6 @@ class App extends Component {
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
-    
   };
 
   render() {
@@ -57,13 +56,24 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
+            const classes = [];
+            if (this.state.persons.length <= 2) {
+              classes.push("red");
+            }
+            if (this.state.persons.length <= 1) {
+              classes.push("bold");
+            }
             return (
-              <Person
-                name={person.name}
-                age={person.age}
-                click={(event) => this.deletePersonHandler(index)}
-                key={person.id}
-              />
+              <div className="App">
+                <h1>Hello</h1>
+                <p className={classes.join(' ')}>This is Real Working</p>
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={(event) => this.deletePersonHandler(index)}
+                  key={person.id}
+                />
+              </div>
             );
           })}
         </div>
